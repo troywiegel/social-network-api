6ddfd5bfcd4468fb6e1e727f4cb87d45d3d7e66d
Troy Wiegel <37552547+troywiegel@users.noreply.github.com>
2022-01-23 21:03:57 -0800

    added friendCount and reactionCount virtuals to User and Thought models

--- models/Thought.js
+++ models/Thought.js
@@ -29,6 +29,11 @@ const thoughtSchema = new Schema(
     }
 )
 
+thoughtSchema.virtual('reactionCount')
+    .get(function () {
+        return `${this.reactions.length}`
+    })
+
 const Thought = model('thought', thoughtSchema)
 
 module.exports = Thought
